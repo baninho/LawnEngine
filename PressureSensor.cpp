432df2ecb223e860f8da40d58d7f28df97e91f80
baninho <florianimbt@gmail.com>
2016-05-21 21:44:10 +0200

    Pressure Sensor: Add Calculation

--- PressureSensor.cpp
+++ PressureSensor.cpp
@@ -4,9 +4,16 @@
 
 PressureSensor::PressureSensor()
 {
+	factor = 0.1;
+	offset = 0;
 }
 
 
 PressureSensor::~PressureSensor()
 {
+}
+
+void PressureSensor::calcIntakePressure()
+{
+	IntakePressure = factor * (float)sensorVoltageReading + offset;
 }
--- PressureSensor.h
+++ PressureSensor.h
@@ -1,8 +1,16 @@
 #pragma once
+extern float IntakePressure;
+
 class PressureSensor
+
 {
 public:
 	PressureSensor();
 	~PressureSensor();
+private:
+	void calcIntakePressure();
+	int sensorVoltageReading;
+	float factor;
+	float offset;
 };
 
