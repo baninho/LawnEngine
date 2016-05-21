d8caf84dd8f3dcc640790c96321cdf77fee9b981
baninho <florianimbt@gmail.com>
2016-05-22 01:27:46 +0200

    Lambda Sensor: Add Calculation

--- LambdaSensor.cpp
+++ LambdaSensor.cpp
@@ -1,12 +1,20 @@
 #include "LambdaSensor.h"
 
-
+bool Rich = false;
+bool Lean = false;
 
 LambdaSensor::LambdaSensor()
 {
+	stoichVoltage = 63;
 }
 
 
 LambdaSensor::~LambdaSensor()
 {
+}
+
+void LambdaSensor::CalcLambdaSensor() 
+{
+	Rich = sensorVoltageReading > stoichVoltage;
+	Lean = !Rich;
 }
--- LambdaSensor.h
+++ LambdaSensor.h
@@ -1,8 +1,16 @@
 #pragma once
+
+extern bool Lean;
+extern bool Rich;
+
 class LambdaSensor
 {
 public:
 	LambdaSensor();
 	~LambdaSensor();
+	void CalcLambdaSensor();
+private:
+	int sensorVoltageReading;
+	int stoichVoltage;
 };
 
